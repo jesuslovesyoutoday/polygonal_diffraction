e6b51aef45386c3823fa2b74c5d3c3e4dd4377a1
jesuslovesyoutoday <jesuslovesyoutoday@yadex.ru>
2022-05-15 15:52:04 +0300

    first implementation with coarse mesh and large integration errors

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import vtk
-from math import sin, cos, pi, sqrt
+from math import sin, cos, pi, sqrt, exp
 import numpy as np
+import quadpy
 from scipy.integrate import quad
 from matplotlib import pyplot as plt
+
+def complex_projection(v1, v2):
+    a = np.array([v1.real, v1.imag]).reshape(1, 2)[0]
+    b = np.array([v2.real, v2.imag]).reshape(1, 2)[0]
+    a_on_b = (np.dot(a, b)/np.dot(b, b)) * b
+    return np.absolute(a_on_b[0] + 1j * a_on_b[1])
+
 
 class Field():
     
@@ -15,11 +23,11 @@ class Field():
         self.R = R              # Radius of the circumscribed circle
         
         self.len = 2*R/sin(pi/N)
-        self.A = np.zeros((N+1, 2), dtype=np.complex) # Coordinates of vertexes
-        self.S = np.zeros((x+1, y+1), dtype=np.complex)   # Coordinates of screen mesh
-        self.E = np.zeros((x+1, y+1), dtype=np.float)   # Distribution of the field 
+        self.A = np.zeros((N+1, 2), dtype=np.complex128) # Coordinates of vertexes
+        self.S = np.zeros((x+1, y+1), dtype=np.complex128)   # Coordinates of screen mesh
+        self.E = np.zeros((x+1, y+1), dtype=np.float64)   # Distribution of the field 
                                                     # on the screen
-        self.I = np.zeros((x, y), dtype=np.float)   # Distribution of the 
+        self.I = np.zeros((x+1, y+1), dtype=np.float64)   # Distribution of the 
                                                     # intensity on the screen
                                                     
         self.x = x              # Amount of horizontal steps
@@ -37,6 +45,7 @@ class Field():
                 self.S[i][j] = x + 1j * y
     
     def make_aperture(self):
+    
         phi = 2*pi/self.N
         for n in range(self.N):
             self.A[n][0] = self.R * cos(n * phi)
@@ -45,6 +54,7 @@ class Field():
         self.A = np.apply_along_axis(lambda args: [complex(*args)], 1, self.A)
     
     def print_aperture(self):
+    
         X = self.S.real
         Y = self.S.imag
         x = self.A.real
@@ -55,9 +65,69 @@ class Field():
         plt.plot(x, y)
         plt.scatter(X, Y)
         plt.grid()
-        plt.show()      
+        plt.show()  
+
+    
+    def make_integral_sum(self, x, y):
+        p = self.S[x][y]
+        summ = 0
+        
+        for j in range(self.N):
+            qj  = self.A[j]
+            qj_ = self.A[j+1]
+            Lj  = qj_ - qj
+            tj  = Lj/np.absolute(Lj)
+            t0  = complex_projection((qj - p), tj)
+            t1  = (np.absolute(Lj) + t0)[0]
+            a = sqrt(np.absolute(qj - p)**2 - t0**2)
+            b = self.k/(2*self.L)
+            I, err = quadpy.quad(lambda x: np.exp(1j * b * x**2) / (x**2 + abs(a)**2), t0, t1, epsabs=10, epsrel=10, limit=495) 
+            #x = t0
+            #print(np.exp(1j * b * x**2) / (x**2 + abs(a)**2))
+            I_ = a * np.exp(1j * b * a**2) * I
+            summ += I_
+        return(summ)
+    
+    
+    def calculate_E(self):
+        
+        for i in range(self.y+1):
+            for j in range(self.x+1):
+                self.E[i][j] = self.U0/(2*pi) * self.make_integral_sum(i, j) 
+    
+    def calculate_I(self): 
+    
+        for i in range(self.y+1):
+            for j in range(self.x+1):
+                self.I[i][j] = self.E[i][j] * np.conj(self.E[i][j])
+        #print(self.I[2][2]) 
+                
+    def snapshot(self):
+    
+        structuredGrid = vtk.vtkStructuredGrid()
+        points = vtk.vtkPoints()
+        E = vtk.vtkDoubleArray()
+        E.SetName("E")
+        
+        for i in range(self.y+1):
+            for j in range(self.x+1):
+                points.InsertNextPoint(self.S[i][j].real, self.S[i][j].imag, 0)
+                E.InsertNextValue(self.I[i][j])
+        
+        structuredGrid.SetDimensions(self.x+1, self.y+1, 1)
+        structuredGrid.SetPoints(points)
+
+        structuredGrid.GetPointData().AddArray(E)
+        
+        writer = vtk.vtkXMLStructuredGridWriter()
+        writer.SetInputDataObject(structuredGrid)
+        writer.SetFileName("diff.vts")
+        writer.Write()
    
-f = Field(1, 6, 1, 0.0000006, 1, 50, 50)
+f = Field(1, 20, 1, 0.0000006, 0.001, 25, 25)
 f.make_aperture()
 f.make_mesh()             
-f.print_aperture()
+#f.print_aperture()
+f.calculate_E()
+f.calculate_I()
+f.snapshot()
